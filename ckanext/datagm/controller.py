cb95d1f814257b7109b9d7f6bc34fee5b6804758
Seb Bacon <seb.bacon@gmail.com>
2011-02-11 11:04:14 +0000

    sort home page layout

--- ckanext/datagm/controller.py
+++ ckanext/datagm/controller.py
@@ -1,10 +1,13 @@
 from ckan.lib.base import *
 import ckan.authz as authz
 from ckan.controllers.home import HomeController
+from ckanext.googleanalytics.controller import GAController
 
 
-class DataGMHomeController(HomeController):
+class DataGMHomeController(HomeController, GAController):
     def index(self):
         c.groups = authz.Authorizer().authorized_query(c.user,
                                                        model.Group)
+        c.top_packages = [x[0] for x in self.get_top_packages()]
+        c.body_class = "home"
         return super(DataGMHomeController, self).index()
