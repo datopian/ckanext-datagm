6e45a4ac77b22d63b4de13aea5d22120ad0c5bf7
Seb Bacon <seb.bacon@gmail.com>
2011-02-18 10:27:53 +0000

    move all controller for homepage into our local version, don't bother inheriting

--- ckanext/datagm/controller.py
+++ ckanext/datagm/controller.py
@@ -1,10 +1,15 @@
+import sys
 from ckan.lib.base import *
+from ckan.lib.search import query_for
 import ckan.authz as authz
-from ckan.controllers.home import HomeController
 from ckanext.googleanalytics.controller import GAController
+from ckan.lib.cache import proxy_cache, get_cache_expires
+
+cache_expires = get_cache_expires(sys.modules[__name__])
 
 
-class DataGMHomeController(HomeController, GAController):
+class DataGMHomeController(GAController):
+    @proxy_cache(expires=cache_expires)
     def index(self):
         c.groups = authz.Authorizer()\
                    .authorized_query(c.user,
@@ -12,4 +17,22 @@ class DataGMHomeController(HomeController, GAController):
                    .order_by(model.Group.title)
         c.top_packages = [x[0] for x in self.get_top_packages()][:5]
         c.body_class = "home"
-        return super(DataGMHomeController, self).index()
+        query = query_for(model.Package)
+        query.run(query='*:*', facet_by=g.facets,
+                  limit=0, offset=0, username=c.user)
+        c.facets = query.facets
+        c.fields = []
+        c.package_count = query.count
+        c.latest_packages = self.authorizer.authorized_query(
+            c.user, model.Package)\
+            .join('revision').order_by(model.Revision.timestamp.desc())\
+            .limit(5).all()
+
+        if len(c.latest_packages):
+            cache_key = str(hash((c.latest_packages[0].id, c.user)))
+        else:
+            cache_key = "fresh-install"
+
+        etag_cache(cache_key)
+        return render('home/index.html', cache_key=cache_key,
+                cache_expire=cache_expires)
