4bab724b80a35a056ffead390a7a7722901f65fc
Seb Bacon <seb.bacon@gmail.com>
2011-02-18 09:47:19 +0000

    order groups alphabetically

--- ckanext/datagm/controller.py
+++ ckanext/datagm/controller.py
@@ -6,8 +6,10 @@ from ckanext.googleanalytics.controller import GAController
 
 class DataGMHomeController(HomeController, GAController):
     def index(self):
-        c.groups = authz.Authorizer().authorized_query(c.user,
-                                                       model.Group)
-        c.top_packages = [x[0] for x in self.get_top_packages()]
+        c.groups = authz.Authorizer()\
+                   .authorized_query(c.user,
+                                     model.Group)\
+                   .order_by(model.Group.title)
+        c.top_packages = [x[0] for x in self.get_top_packages()][:5]
         c.body_class = "home"
         return super(DataGMHomeController, self).index()
