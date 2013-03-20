7b0290249a53ee32ef16fb69f48261822b07ec3d
Sean Hammond <seanhammond@lavabit.com>
2013-03-20 12:28:43 +0100

    setup.py retab and clean up
    This doesn't actually change anything just fixes the indentation and
    alignment, removes unused imports, etc.

--- setup.py
+++ setup.py
@@ -1,33 +1,30 @@
 from setuptools import setup, find_packages
-import sys, os
 
 version = '0.1'
 
 setup(
-	name='ckanext-datagm',
-	version=version,
-	description="Customisations of CKAN for DataGM",
-	long_description="""\
-	""",
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords='',
-	author='Seb Bacon',
-	author_email='seb.bacon@gmail.com',
-	url='',
-	license='',
-	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext', 'ckanext.datagm'],
-	include_package_data=True,
-	zip_safe=False,
-	install_requires=[
-		# -*- Extra requirements: -*-
-	],
-	entry_points=\
-	"""
+    name='ckanext-datagm',
+    version=version,
+    description="Customisations of CKAN for DataGM",
+    long_description="""\
+    """,
+    classifiers=[],
+    keywords='',
+    author='Seb Bacon',
+    author_email='seb.bacon@gmail.com',
+    url='',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.datagm'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+    ],
+    entry_points='''
         [ckan.plugins]
-	datagm=ckanext.datagm.plugin:DataGMPlugin
+        datagm=ckanext.datagm.plugin:DataGMPlugin
 
         [ckan.forms]
         datagm_package_form = ckanext.datagm.package_form:get_datagm_fieldset
-	""",
+    ''',
 )
