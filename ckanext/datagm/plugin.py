a768b818bcbf1d6395fa1c380a6660cd44bfa581
Sean Hammond <seanhammond@lavabit.com>
2013-03-20 14:32:18 +0100

    Start CKAN 2.0 version of this plugin
    Basically delete everything and start again. So far the new 2.0 version
    just has a custom template that tweaks the header links, that's all.

--- ckanext/datagm/plugin.py
+++ ckanext/datagm/plugin.py
@@ -1,60 +1,12 @@
-import os
-from logging import getLogger
-
-from genshi.filters.transform import Transformer
-from genshi.input import HTML
-
-from ckan.plugins import implements, SingletonPlugin
-from ckan.plugins import IRoutes, IConfigurer
-from ckan.plugins import IGenshiStreamFilter
-
-log = getLogger(__name__)
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as tk
 
 
-class DataGMPlugin(SingletonPlugin):
-    implements(IRoutes, inherit=True)
-    implements(IConfigurer, inherit=True)
-    implements(IGenshiStreamFilter, inherit=True)
-
-    def before_map(self, map):
-        map.connect('home', '/',
-                    controller='ckanext.datagm.controller:DataGMHomeController',
-                    action='index')
-        map.connect('/user/register',
-                    controller='ckanext.datagm.controller:DataGMUserController',
-                    action='datagm_register')
-        return map
+class DataGMPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer, inherit=False)
 
     def update_config(self, config):
-        here = os.path.dirname(__file__)
-        rootdir = os.path.dirname(os.path.dirname(here))
-        our_public_dir = os.path.join(rootdir, 'ckanext',
-                                      'datagm', 'theme', 'public')
-        template_dir = os.path.join(rootdir, 'ckanext',
-                                    'datagm', 'theme', 'templates')
-        config['extra_public_paths'] = ','.join(
-            [path
-             for path in [our_public_dir, config.get('extra_public_paths', '')]
-             if path]
-             )
-        log.debug('extra_public_paths = %r', config['extra_public_paths'])
-        config['extra_template_paths'] = ','.join(
-            [path
-             for path in [template_dir, config.get('extra_template_paths', '')]
-             if path]
-            )
-        config['googleanalytics.id'] = 'UA-21313878-1'
-        config['ckan.site_title'] = "DataGM - Data Greater Manchester"
-        config['ckan.site_logo'] = "/img/datagm-beta.png"
-        config['ckan.default_roles.Package'] = \
-                 ('{"visitor": ["reader"], '
-                  '"logged_in": ["reader"]} ')
 
-    def filter(self, stream):
-        text_containers = ["p", "a", "h1", "h2", "h3", "h4", "em",
-                           "strong"]
-        text_xpath = "|".join(["//%s/text()" % x \
-                               for x in text_containers])
-        stream = stream | Transformer(text_xpath)\
-                 .substitute(r'([^/])[pP]ackage', r'\1dataset')
-        return stream
+        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # that CKAN will use this plugin's custom templates.
+        tk.add_template_directory(config, 'templates')
